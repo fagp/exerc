8fb79bd9d76fe36697b9392e564e5e0dd8b411e9
Fidel Guerrero Pena <fagp@cin.ufpe.br>
2021-03-01 17:59:30 -0300

    init

--- lista3_1/__init__.py
+++ lista3_1/__init__.py
@@ -1,8 +1,8 @@
-from datasets import (
+from .datasets import (
     MNIST_train,
     MNIST_test,
     scaled_MNIST_train,
     scaled_MNIST_test,
 )
 
-from models import train_model
+from .models import train_model
