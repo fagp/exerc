8c56c654a6899a51e40267e3389d900d91bbd1ab
Fidel Guerrero Pena <fagp@cin.ufpe.br>
2021-03-03 20:08:34 -0300

    modify add image

--- lista3_2/eval.py
+++ lista3_2/eval.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 import torch.nn.functional as F
 from skimage.metrics import structural_similarity as ssim
 
@@ -8,15 +9,29 @@ def wrap_input(image):
 
 
 def unwrap_output(pred):
-    return pred[0].detach().cpu().numpy()
+    return pred.detach().cpu().numpy().transpose((2, 1, 0))
 
 
 def compute_test_ssim(test_dataset, model):
-    ssim = []
+    ssim_val = []
     model.eval()
+    device = (
+        torch.device("cuda:0")
+        if torch.cuda.is_available()
+        else torch.device("cpu")
+    )
+    model.to(device)
     for image, target in test_dataset:
-        pred = model(wrap_input(image))
-        pred = unwrap_output(pred)
-        ssim.append(ssim(target, pred, data_range=pred.max() - pred.min()))
+        pred = model(wrap_input(image).to(device))
+        pred = unwrap_output(pred[0])
+        target = unwrap_output(target)
+        ssim_val.append(
+            ssim(
+                target,
+                pred,
+                data_range=pred.max() - pred.min(),
+                multichannel=True,
+            )
+        )
 
-    return np.array(ssim).mean()
+    return np.array(ssim_val).mean()
