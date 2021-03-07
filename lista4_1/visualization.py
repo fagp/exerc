83691861f5219e791b8ee17dad0d8c82b6a3c2ca
Fidel Guerrero Pena <fagp@cin.ufpe.br>
2021-03-07 10:51:57 -0300

    including lista 4

--- lista4_1/visualization.py
+++ lista4_1/visualization.py
@@ -1,5 +1,6 @@
 import torch
 import random
+import imageio
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
@@ -33,38 +34,48 @@ def visualize_space(dataset, net, device):
     return torch.from_numpy(vimage[:, :, :3].transpose((2, 1, 0)))
 
 
+def plot_to_image(figure):
+    plt.savefig("temporal.png")
+    plt.close(figure)
+    tuple_image = imageio.imread("temporal.png")
+    return tuple_image
+
+
 def visualize_tuple(dataset, net, device):
     index = random.randint(0, len(dataset))
     anchor, positives, negatives = dataset[index]
 
-    emb_0 = net(anchor.unsqueeze(0).to(device).float()).detach().cpu().numpy()
-    emb_1 = (
-        net(positives.unsqueeze(0).to(device).float()).detach().cpu().numpy()
-    )
-    emb_2 = (
-        net(negatives.unsqueeze(0).to(device).float()).detach().cpu().numpy()
-    )
+    emb_0 = net(anchor.unsqueeze(0).to(device).float()).detach()
+    emb_1 = net(positives.unsqueeze(0).to(device).float()).detach()
+    emb_2 = net(negatives.unsqueeze(0).to(device).float()).detach()
 
     pos_dist = torch.nn.functional.pairwise_distance(emb_0, emb_1)
     neg_dist = torch.nn.functional.pairwise_distance(emb_0, emb_2)
 
-    fig = Figure()
-    canvas = FigureCanvas(fig)
-    # ax = fig.gca()
-    plt.subplot(1, 3, 1)
+    figure = plt.figure()
+    plt.subplot(1, 3, 1, title="Anchor")
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
     plt.imshow(anchor.numpy().transpose(2, 1, 0) * 255)
-    plt.title("Anchor")
 
-    plt.subplot(1, 3, 2)
+    plt.subplot(
+        1, 3, 2, title="Positive distance: {:1.4f}".format(pos_dist.item())
+    )
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
     plt.imshow(positives.numpy().transpose(2, 1, 0) * 255)
-    plt.title("Positive distance: {:1.4f}".format(pos_dist.item()))
 
-    plt.subplot(1, 3, 3)
+    plt.subplot(
+        1, 3, 3, title="Negative distance: {:1.4f}".format(neg_dist.item())
+    )
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
     plt.imshow(negatives.numpy().transpose(2, 1, 0) * 255)
-    plt.title("Negative distance: {:1.4f}".format(neg_dist.item()))
-    canvas.draw()
 
-    s, (width, height) = canvas.print_to_buffer()
-    vimage = np.fromstring(s, dtype="uint8").reshape(height, width, 4)
+    vimage = plot_to_image(figure)
+    print(vimage.shape)
 
     return torch.from_numpy(vimage[:, :, :3].transpose((2, 1, 0)))
